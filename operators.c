6eb07940ea732d58ee71fb61d58b89d536d9ddc9
karorimesh <karorimesh@gmail.com>
2021-09-28 10:19:14 +0300

    C lecture 2

---
+++ operators.c
@@ -0,0 +1,12 @@
+//#include<stdio.h>
+//int main(){
+//    int i=3,j=7,k;
+//    char c = 'a';
+//    k=i+j;
+//    k++;
+//    ++k;
+//
+//     printf("sum is %d\n",k);
+////    printf("Character%c\n",c);
+//    return 0;
+//}
